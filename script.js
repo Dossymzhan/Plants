46ac5103c64fa9e80b04f23c5b9d02e6acc13741
Dossymzhan <dosymjan6870@gmail.com>
2023-02-20 17:58:52 +0300

    feature: toggle menu hambuerger

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hamburger menu toggle
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".navbar");
 
@@ -6,3 +7,18 @@ hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
 
+// Hamburger menu disappear click on links
+document.querySelectorAll(".nav-list").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navBar.classList.remove("active");
+  })
+);
+
+// Hamburger menu disappear on click window
+document.addEventListener("click", (e) => {
+  if (e.target.classlist.contains("navbar")) {
+    hamburger.classList.remove("active");
+    navBar.classList.remove("active");
+  }
+});
